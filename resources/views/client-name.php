ffd5616a4aea7fa137ae6c5654ee9fefd398c213
Marcos Barbosa <barbosa.mg@gmail.com>
2017-02-28 00:17:04 -0300

    Exemplos de utilização de rotas e views

---
+++ resources/views/client-name.php
@@ -0,0 +1,1 @@
+<p>Client <?php echo $id ?>, <?php echo $name ?></p>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +16,35 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/minharota', function() {
+	return view('hello');
+});
+
+
+Route::get('/client', function() {
+	return view('client');
+});
+
+Route::get('/client/{id}/{name?}', function(Request $request, $id, $name = 'Visitante') {
+// 	return view('client-name', [
+// 		'id'=>$id,
+// 		'name'=>$name
+// 	]);
+
+	return view('client-name')
+		->with('id', $id)
+		->with('name', $name);
+	
+});
+	
+
+Route::post('/client', function(Request $request) {
+	return $request->get('value');
+})->name('client.store');
+	
+
+
+// Route::get('/minharota/{nome?}', function($nome = 'Visitante') {
+// 	return "Olá $nome";
+// });
